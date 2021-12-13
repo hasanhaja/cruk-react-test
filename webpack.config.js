20c1439c0e7217d928ca3be77104fc68a9468305
Hasan Ali <hasanhaja@gmail.com>
2021-12-13 22:37:59 +0400

    Configure TS and Webpack to starting template

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+  mode: isProd ? "production" : "development",
+  entry: {
+    index: "./src/index.tsx",
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ].concat(isProd ? [new MiniCssExtractPlugin()] : []),
+};
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 9000,
+    open: true,
+    hot: "only",
+    // compress: true,
+    devMiddleware: {
+        stats: "errors-only",
+    },
+    client: {
+        overlay: true,
+    },
+  };
+}
+
+module.exports = config;
